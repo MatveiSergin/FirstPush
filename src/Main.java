b7961d9a573d5c4b2c8a2a5961b46d861d6b39b0
Matvey <matvey.sergin04@yandex.ru>
2023-11-01 17:11:30 +0300

    fixed task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Order order = new Order(10);
+        System.out.println(order.toString());
     }
 }
